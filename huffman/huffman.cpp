5e6062cff6ee3c57b9a33c055c2bcce673f06e6c
Usell2021 <gameloser366@gmail.com>
2024-12-01 15:13:25 +0500

    1

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -5,25 +5,27 @@ FILE *f;
 struct Forest {
     int weight;
     int root;
+    int symbol;
 };
 
 struct Tree {
     int left;
     int right;
     int parent;
+    int symbol;
 };
 
 std::pair<int, int> getMinsIdx(Forest f[], int s) {
     Forest wm = {
-        .weight = 0,
-        .root = 0,
+            .weight = 0,
+            .root = 0,
     };
 
     for (int i = 0; i < s; i++) {
         if (wm.weight < f[i].weight)
             wm = {
-                .weight = f[i].weight,
-                .root = i
+                    .weight = f[i].weight,
+                    .root = i
             };
     }
     Forest a = wm, b = wm;
@@ -31,8 +33,8 @@ std::pair<int, int> getMinsIdx(Forest f[], int s) {
     for (int i = 0; i < s; i++) {
         if (a.weight > f[i].weight) {
             a = {
-                .weight = f[i].weight,
-                .root = i
+                    .weight = f[i].weight,
+                    .root = i
             };
         }
     }
@@ -40,29 +42,34 @@ std::pair<int, int> getMinsIdx(Forest f[], int s) {
     for (int i = 0; i < s; i++) {
         if (f[i].weight <= b.weight && a.root != i) {
             b = {
-                .weight = f[i].weight,
-                .root = i
+                    .weight = f[i].weight,
+                    .root = i
             };
         }
     }
     return {a.root, b.root};
 }
 
-void getKeys(Tree t[], int ks[], int p, int k) {
-    if (t[p].left == t[p].right == -1) {
-        ks[p] = k;
-    } else if (t[p].left != -1) {
-        getKeys(t, ks, t[p].left, k << 1);
-    } else if (t[p].right != -1) {
-        getKeys(t, ks, t[p].right, k << 1 + 1);
-    }
-}
+
 
 int frequency[256];
 Forest forest[256];
 Tree tree[512];
 int tree_size;
 int forest_size;
+std::pair<int, int> keys[256];
+
+void getKeys(Tree t[], int p, int k, int l) {
+    if (t[p].left == t[p].right) {
+        keys[p].first = k;
+        keys[p].second = l;
+    } else if (t[p].left != -1) {
+        getKeys(t, t[p].left, k * 10, l+1);
+    }
+    if (t[p].right != -1) {
+        getKeys(t, t[p].right, k * 10 + 1, l+1);
+    }
+}
 
 int main() {
     f = fopen("./input.txt", "rb");
@@ -72,11 +79,13 @@ int main() {
         frequency[ch]++;
     }
 
-    for (const auto &item: frequency) {
+    for (int i = 0; i < 256; i++) {
+        auto item = frequency[i];
         if (item > 0) {
             forest[forest_size] = {
-                .weight = item,
-                .root = forest_size
+                    .weight = item,
+                    .root = forest_size,
+                    .symbol = i,
             };
 
             forest_size++;
@@ -85,9 +94,10 @@ int main() {
 
     for (auto &item: tree) {
         item = {
-            .left = -1,
-            .right = -1,
-            .parent = -1,
+                .left = -1,
+                .right = -1,
+                .parent = -1,
+                .symbol = -1,
         };
     }
 
@@ -99,24 +109,26 @@ int main() {
 
         int tr = tree_size - 1;
         Forest ft = {
-            .weight = fa.weight + fb.weight,
-            .root = tr
+                .weight = fa.weight + fb.weight,
+                .root = tr
         };
 
         tree[fa.root] = {
-            .left = tree[fa.root].left,
-            .right = tree[fa.root].right,
-            .parent = tr
+                .left = tree[fa.root].left,
+                .right = tree[fa.root].right,
+                .parent = tr,
+                .symbol = fa.symbol,
         };
         tree[fb.root] = {
-            .left = tree[fb.root].left,
-            .right = tree[fb.root].right,
-            .parent = tr
+                .left = tree[fb.root].left,
+                .right = tree[fb.root].right,
+                .parent = tr,
+                .symbol = fb.symbol,
         };
         tree[tr] = {
-            .left = fa.root,
-            .right = fb.root,
-            .parent = tree[tr].parent
+                .left = fa.root,
+                .right = fb.root,
+                .parent = tree[tr].parent
         };
 
 
@@ -132,7 +144,20 @@ int main() {
         forest_size--;
     }
 
-    for (int i = 0; i < 12; i++) {
-        std::cout << tree[i].left << ' ' << tree[i].right << ' ' << tree[i].parent << '\n';
+    getKeys(tree, tree_size - 1, 0, 0);
+
+    for (int i = 0; i < 256; i++) {
+        auto item = keys[i];
+        if (item.second != 0)
+        std::cout << char(tree[i].symbol) << ' ' << item.first << ' ' << item.second << '\n';
     }
+
+    f = fopen("./input.txt", "rb");
+    while (fscanf(f, "%c", &ch) != -1) {
+        frequency[ch]++;
+    }
+
+
+    std::cout << "norm";
+
 }
